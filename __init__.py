1e40681b23b0e94f73ddcbd52d2c72431568b759
Glen Cook <glen@glencook.net>
2024-12-01 19:41:06 -0800

    ludpate

--- __init__.py
+++ __init__.py
@@ -1,25 +1,23 @@
-from __future__ import annotations
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.const import CONF_MAC
+from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
-from .triones import TrionesInstance
-
-PLATFORMS = ["light"]
+PLATFORMS = ["light", "sensor"]  # Add the platforms your integration supports
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Triones from a config entry."""
-    instance = TrionesInstance(entry.data[CONF_MAC])
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = instance
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.data.setdefault("triones", {})
+    hass.data["triones"][entry.entry_id] = entry.data
+
+    # Forward the entry setup to the platforms
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        instance = hass.data[DOMAIN].pop(entry.entry_id)
-        await instance.disconnect()
+        hass.data["triones"].pop(entry.entry_id)
+
     return unload_ok
