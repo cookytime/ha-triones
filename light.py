2a0e2b7618e47a717f3e4ecb7dd6d6773951e2ee
Glen Cook <glen@glencook.net>
2024-12-01 22:56:24 -0800

    test

--- light.py
+++ light.py
@@ -1,33 +1,27 @@
-import logging
-import voluptuous as vol
-from typing import Any, Optional, Tuple
-
+from homeassistant.components.light import LightEntity
 from .triones import TrionesInstance
-from .const import DOMAIN
-
-from homeassistant.const import CONF_MAC
-import homeassistant.helpers.config_validation as cv
-from homeassistant.components.light import (COLOR_MODE_RGB, PLATFORM_SCHEMA,
-                                            LightEntity, ATTR_RGB_COLOR, ATTR_BRIGHTNESS, COLOR_MODE_WHITE, ATTR_WHITE)
-from homeassistant.util.color import (match_max_scale)
-from homeassistant.helpers import device_registry
-
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_MAC): cv.string
-})
 
 async def async_setup_entry(hass, config_entry, async_add_devices):
-    instance = hass.data[DOMAIN][config_entry.entry_id]
-    async_add_devices([TrionesLight(instance, config_entry.data["name"], config_entry.entry_id)])
+    """Set up Triones lights from a config entry."""
+    # Extract data from the config entry
+    mac_address = config_entry.data["mac"]
+    name = config_entry.data["name"]
+
+    # Ensure instance is properly initialized
+    instance = TrionesInstance(mac_address)
+
+    # Add the light device
+    async_add_devices([TrionesLight(instance, name, config_entry.entry_id)])
+
 
 class TrionesLight(LightEntity):
     def __init__(self, instance: TrionesInstance, name: str, entry_id: str):
-        """Initialize a Triones light."""
+        """Initialize the Triones light."""
         self._instance = instance
         self._name = name
         self._entry_id = entry_id
 
-        # Set attributes
+        # Attributes
         self._attr_unique_id = self._instance.mac
         self._attr_name = self._name
         self._is_on = None
@@ -65,4 +59,3 @@ class TrionesLight(LightEntity):
     def rgb_color(self):
         """Return the RGB color value."""
         return self._rgb_color
-    
